17906e70b7c7bd13efb08848601d048ae9629e82
Niyati Nehal <niyatinehal58@gmail.com>
2023-01-10 22:59:40 +0530

    question 12:completed

---
+++ Goldman sachs/ques12.cpp
@@ -0,0 +1,22 @@
+//1305. All Elements in Two Binary Search Trees
+//https://leetcode.com/problems/all-elements-in-two-binary-search-trees/submissions/875594214/
+class Solution
+{
+public:
+     vector<int> res;
+     void dfs(TreeNode *root)
+     {
+          if (!root)
+               return;
+          res.push_back(root->val);
+          dfs(root->left);
+          dfs(root->right);
+     }
+     vector<int> getAllElements(TreeNode *root1, TreeNode *root2)
+     {
+          dfs(root1);
+          dfs(root2);
+          sort(begin(res), end(res));
+          return res;
+     }
+};
