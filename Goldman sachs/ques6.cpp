a8e94af599f34e09df9c003e2398fffc496f388e
Niyati Nehal <niyatinehal58@gmail.com>
2023-01-10 22:06:09 +0530

    ques6:completed

---
+++ Goldman sachs/ques6.cpp
@@ -0,0 +1,24 @@
+//2260. Minimum Consecutive Cards to Pick Up
+//https://leetcode.com/problems/minimum-consecutive-cards-to-pick-up/description/
+class Solution {
+public:
+    int minimumCardPickup(vector<int>&s) {
+
+       int maxi = INT_MAX;
+       unordered_map<int,int>mpp;
+       int n = s.size();
+
+       for(int i = 0; i < n; ++i)
+       {   
+
+           if(mpp.find(s[i]) != mpp.end())  maxi = min(maxi, i-mpp[s[i]]+1);
+
+             mpp[s[i]] = i;
+       }
+
+      if(maxi == INT_MAX) return -1;
+
+      return maxi;
+      
+    }
+};
