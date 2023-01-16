298d6516a68e6e6f7188e0228f8751d4819eb927
Niyati Nehal <niyatinehal58@gmail.com>
2023-01-16 23:40:29 +0530

    ques11 to 15 :completed

---
+++ Adobe/ques12.cpp
@@ -0,0 +1,21 @@
+// 581. Shortest Unsorted Continuous Subarray
+// https://leetcode.com/problems/shortest-unsorted-continuous-subarray/description/
+class Solution
+{
+public:
+    int findUnsortedSubarray(vector<int> &nums)
+    {
+        vector<int> sorted(nums);
+        sort(sorted.begin(), sorted.end());
+        int n = nums.size(), i = 0, j = n - 1;
+        while (i < n && nums[i] == sorted[i])
+        {
+            i++;
+        }
+        while (j > i && nums[j] == sorted[j])
+        {
+            j--;
+        }
+        return j + 1 - i;
+    }
+};
